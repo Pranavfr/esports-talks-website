e72b2b09a8f286512eee00475f791fe2b0ee132f
Pranav Rathore <pranav16022016@gmail.com>
2025-07-30 15:34:25 +0530

    Fix Stripe API version

--- app/api/create-payment-intent/route.ts
+++ app/api/create-payment-intent/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 
 // Initialize Stripe with your secret key
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2023-10-16' // Use the latest API version
+  apiVersion: '2025-06-30.basil' // Use the latest API version
 })
 
 export async function POST(request: Request) {
